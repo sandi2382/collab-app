f62f650df15ab7f368b903357cb63d53917e0b55
Rob J. Calderon <rob.jcalderon@gmail.com>
2020-09-30 18:09:48 -0700

    chartbot-chat updates

--- src/components/ChannelList.js
+++ src/components/ChannelList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -6,16 +6,17 @@ import {
   TextInput,
   StyleSheet,
   SectionList,
-} from 'react-native';
-import {ChannelListItem} from './ChannelListItem';
+  Image
+} from "react-native";
+import { ChannelListItem } from "./ChannelListItem";
 
-export const ChannelList = ({client, changeChannel}) => {
+export const ChannelList = ({ client, changeChannel }) => {
   const {
     activeChannelId,
     setActiveChannelId,
     unreadChannels,
     readChannels,
-    oneOnOneConversations,
+    oneOnOneConversations
   } = useWatchedChannels(client, changeChannel);
 
   const renderChannelRow = (channel, isUnread) => {
@@ -41,18 +42,30 @@ export const ChannelList = ({client, changeChannel}) => {
     <SafeAreaView>
       <View style={styles.container}>
         <View style={styles.headerContainer}>
-          <TextInput
-            style={styles.inputSearchBox}
-            placeholderTextColor="grey"
-            placeholder="Jump to"
-          />
+          <View style={{ paddingLeft: 10 }}>
+            <Image
+              style={{
+                //flex: 1,
+                aspectRatio: Platform.OS === "ios" ? 3.0 : 3.0,
+                resizeMode: "contain",
+                height: 80,
+                width: 100
+              }}
+              source={require("../../assets/chartbottextlogo.png")}
+            />
+            <TextInput
+              style={styles.inputSearchBox}
+              placeholderTextColor="grey"
+              placeholder="Search"
+            />
+          </View>
         </View>
 
         <SectionList
           style={styles.sectionList}
           sections={[
             {
-              title: 'Unread',
+              title: "Unread",
               /**
                * As you can note here, I have supplied `isUnread: true` to unread section data.
                * This way I can tell the renderChannelRow function if the current channel to render
@@ -62,24 +75,24 @@ export const ChannelList = ({client, changeChannel}) => {
                * - which basically loops through messages.
                */
               isUnread: true,
-              data: unreadChannels || [],
+              data: unreadChannels || []
             },
             {
-              title: 'Channels',
+              title: "Channels",
               isUnread: false,
-              data: readChannels || [],
+              data: readChannels || []
             },
             {
-              title: 'Direct Messages',
+              title: "Direct Messages",
               isUnread: false,
-              data: oneOnOneConversations || [],
-            },
+              data: oneOnOneConversations || []
+            }
           ]}
           keyExtractor={(item, index) => item.id + index}
-          renderItem={({item, section}) => {
+          renderItem={({ item, section }) => {
             return renderChannelRow(item, section.isUnread);
           }}
-          renderSectionHeader={({section: {title}}) => (
+          renderSectionHeader={({ section: { title } }) => (
             <View style={styles.groupTitleContainer}>
               <Text style={styles.groupTitle}>{title}</Text>
             </View>
@@ -96,15 +109,15 @@ const useWatchedChannels = (client, changeChannel) => {
   const [oneOnOneConversations, setOneOnOneConversations] = useState([]);
   const [hasMoreChannels, setHasMoreChannels] = useState(true);
   const filters = {
-    type: 'messaging',
-    example: 'slack-demo',
+    type: "messaging",
+    example: "slack-demo",
     members: {
-      $in: [client.user.id],
-    },
+      $in: [client.user.id]
+    }
   };
 
-  const sort = {has_unread: -1, cid: -1};
-  const options = {limit: 30, state: true};
+  const sort = { has_unread: -1, cid: -1 };
+  const options = { limit: 30, state: true };
 
   useEffect(() => {
     if (!hasMoreChannels) {
@@ -127,11 +140,11 @@ const useWatchedChannels = (client, changeChannel) => {
     async function fetchChannels() {
       const channels = await client.queryChannels(filters, sort, {
         ...options,
-        offset,
+        offset
       });
 
       offset = offset + channels.length;
-      channels.forEach((c) => {
+      channels.forEach(c => {
         if (c.countUnread() > 0) {
           _unreadChannels.push(c);
         } else if (Object.keys(c.state.members).length === 2) {
@@ -159,12 +172,12 @@ const useWatchedChannels = (client, changeChannel) => {
 
   useEffect(() => {
     function handleEvents(e) {
-      if (e.type === 'message.new') {
+      if (e.type === "message.new") {
         const cid = e.cid;
 
         // Check if the channel (which received new message) exists in group channels.
         const channelReadIndex = readChannels.findIndex(
-          (channel) => channel.cid === cid,
+          channel => channel.cid === cid
         );
 
         if (channelReadIndex >= 0) {
@@ -177,7 +190,7 @@ const useWatchedChannels = (client, changeChannel) => {
 
         // Check if the channel (which received new message) exists in oneOnOneConversations list.
         const oneOnOneConversationIndex = oneOnOneConversations.findIndex(
-          (channel) => channel.cid === cid,
+          channel => channel.cid === cid
         );
         if (oneOnOneConversationIndex >= 0) {
           // If yes, then remove it from oneOnOneConversations list and add it to unreadChannels list
@@ -189,7 +202,7 @@ const useWatchedChannels = (client, changeChannel) => {
 
         // Check if the channel (which received new message) already exists in unreadChannels.
         const channelUnreadIndex = unreadChannels.findIndex(
-          (channel) => channel.cid === cid,
+          channel => channel.cid === cid
         );
         if (channelUnreadIndex >= 0) {
           const channel = unreadChannels[channelUnreadIndex];
@@ -199,14 +212,14 @@ const useWatchedChannels = (client, changeChannel) => {
         }
       }
 
-      if (e.type === 'message.read') {
+      if (e.type === "message.read") {
         if (e.user.id !== client.user.id) {
           return;
         }
         const cid = e.cid;
         // get channel index
         const channelIndex = unreadChannels.findIndex(
-          (channel) => channel.cid === cid,
+          channel => channel.cid === cid
         );
         if (channelIndex < 0) {
           return;
@@ -241,39 +254,43 @@ const useWatchedChannels = (client, changeChannel) => {
     readChannels,
     setReadChannels,
     oneOnOneConversations,
-    setOneOnOneConversations,
+    setOneOnOneConversations
   };
 };
 
 const styles = StyleSheet.create({
   container: {
     paddingLeft: 5,
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    height: '100%',
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    height: "100%"
   },
   headerContainer: {
-    padding: 10,
-    marginRight: 10,
+    //padding: 10,
+    //marginRight: 10,
+    justifyContent: "center",
+    textAlign: "center",
+    marginRight: 50
   },
   inputSearchBox: {
-    backgroundColor: '#2e0a2f',
+    backgroundColor: "#AAB8C2",
     padding: 10,
+    borderRadius: 25
   },
   sectionList: {
     flexGrow: 1,
-    flexShrink: 1,
+    flexShrink: 1
   },
   groupTitleContainer: {
     padding: 10,
-    borderBottomColor: '#995d9a',
+    borderBottomColor: "#F5F8FA",
     borderBottomWidth: 0.3,
-    marginBottom: 7,
+    marginBottom: 7
   },
   groupTitle: {
-    color: 'white',
-    fontWeight: '100',
+    color: "white",
+    fontWeight: "100",
     fontSize: 12,
-    fontFamily: 'Lato-Regular',
-  },
+    fontFamily: "Lato-Regular"
+  }
 });
